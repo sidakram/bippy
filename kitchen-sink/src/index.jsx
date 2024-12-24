72b14d54a87b2ab49a850f47f31ce9c6ba69ecf8
Aiden Bai <aiden.bai05@gmail.com>
2024-12-24 05:29:58 -0800

    refactor(index): Simplify component import statement

--- kitchen-sink/src/index.jsx
+++ kitchen-sink/src/index.jsx
@@ -6,7 +6,7 @@ const PROJECT = "main";
 const render = async () => {
 	const Component = (
 		await import(
-			`./${process.env.NODE_ENV !== "production" ? PROJECT : "main"}.jsx`
+			process.env.NODE_ENV !== "production" ? `./${PROJECT}.jsx` : "./main.jsx"
 		)
 	).default;
 	ReactDOM.createRoot(document.getElementById("root")).render(<Component />);
