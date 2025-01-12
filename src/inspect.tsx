091bdf0b49e48d63ebf277608a12ddda00275430
Asuka109 <asuka109@foxmail.com>
2025-01-12 21:54:30 +0800

    fix(inspector): z-index transition (#12)

--- src/inspect.tsx
+++ src/inspect.tsx
@@ -280,7 +280,7 @@ export const Inspector = ({
           padding: '1ch',
           width: '50ch',
           height: '40ch',
-          transition: 'all 150ms',
+          transition: 'all 150ms, z-index 0ms',
           overflow: 'visible',
           boxShadow: '0 1px 3px 0 rgb(0 0 0 / 0.1)',
           border: '1px solid black',
